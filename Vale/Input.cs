dafa551d37813ec0deae3807e2d2e94605603f39
Spencer Corkran <corkransp@gmail.com>
2015-05-20 13:20:58 -0400

    Input Manager skeleton

---
+++ Vale/Input.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Vale
+{
+    static class Input
+    {
+        public static KeyboardState currentKeyboardState;
+        public static KeyboardState previousKeyboardState;
+
+        public static GamePadState currentGamePadState;
+        public static GamePadState previousGamePadState;
+
+        public static MouseState currentMouseState;
+        public static MouseState previousMouseState;
+
+        public static void Update()
+        {
+            previousKeyboardState = currentKeyboardState;
+            previousMouseState = currentMouseState;
+            previousGamePadState = currentGamePadState;
+
+            currentKeyboardState = Keyboard.GetState();
+            currentMouseState = Mouse.GetState();
+            currentGamePadState = GamePad.GetState(0);
+        }
+    }
+}
--- Vale/Player.cs
+++ Vale/Player.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Vale
 {
@@ -16,6 +17,11 @@ namespace Vale
         }
 
         public void Update()
+        {
+
+        }
+
+        private void getInput()
         {
 
         }
