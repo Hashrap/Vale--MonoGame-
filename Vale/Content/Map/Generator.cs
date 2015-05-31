b829d47d88b7b339f279735bbb82d5b2232e6417
Spencer Corkran <corkransp@gmail.com>
2015-05-31 12:05:16 -0400

    Map Generator implemented but not integrated

---
+++ Vale/Content/Map/Generator.cs
@@ -0,0 +1,196 @@
+﻿//Spencer Corkran
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Vale.Content.Map
+{
+    class Generator
+    {
+        //Attributes and objects
+        public enum DungeonType { Cave, Dungeon, Forest };
+        public DungeonType type;
+        public Map[] arrayOfLevels;
+        public string name;
+        //Constructor
+        public Generator(int itype, string name)
+        {
+            switch (itype)
+            {
+                case 0:
+                    type = DungeonType.Cave;
+                    break;
+                case 1:
+                    type = DungeonType.Dungeon;
+                    break;
+                case 2:
+                    type = DungeonType.Forest;
+                    break;
+            }
+            this.name = name;
+        }
+
+        public Generator() { }
+
+        public void Cave(int levels, int algorithm1, int algorithm2, int wall, int size_x, int size_y)
+        {
+            arrayOfLevels = new CaveMap[levels];
+            for (int i = 0; i < levels; i++)
+            {
+                CaveMap cl;
+                bool good = false;
+                while (good != true)
+                {
+                    cl = new CaveMap(size_y, size_x);
+                    cl.randomFill(wall);
+
+                    for (int j = 0; j < algorithm1; j++) //ages the cells using the 4-5 | 2 rule
+                        cl.ageDungeon(2);
+                    for (int j = 0; j < algorithm2; j++)//ages the cells using the 4-5 rule
+                        cl.ageDungeon(1);
+                    arrayOfLevels[i] = cl;
+
+                    /*These check the validity of the map, trashes "bad" maps until it gets a good one
+                    * Will attempt to repair mildly disjointed rooms.*/
+                    arrayOfLevels[i].floodPrep();
+                    int[] fTile = arrayOfLevels[i].findFloor();
+                    arrayOfLevels[i].floodSearch(fTile[0], fTile[1]);
+                    if (arrayOfLevels[i].isEverythingReachable() == false && arrayOfLevels[i].BadCount > size_x + size_y)
+                    {
+                        good = false; //Trashes the map
+                    }
+                    else if (arrayOfLevels[i].isEverythingReachable() == false && arrayOfLevels[i].BadCount <= size_x + size_y)
+                    {
+                        arrayOfLevels[i].fill(); //Repairs the map
+                        good = true;
+                    }
+                    else
+                        good = true; //Map is fine as is
+                    //arrayOfLevels[i].placeObject();
+                    int waiter = 0;
+                    while (waiter < 10000000)
+                    {
+                        waiter++; //prevents duplicates (using the same seed twice)
+                    }
+                }
+            }
+        }
+
+        public void Dungeon(int levels, double pos_min, double pos_max, int min_room, int iterations, int size_y, int size_x)
+        {
+            arrayOfLevels = new DungeonMap[levels];
+            for (int i = 0; i < levels; i++)
+            {
+                DungeonMap dl = new DungeonMap(size_y, size_x);
+                dl.dungeonGen(dl.board, iterations, pos_min, pos_max);
+                arrayOfLevels[i] = dl;
+            }
+        }
+        public void Forest(int size_y, int size_x)
+        {
+            //Code to generate a forest
+        }
+
+        //This should save the dungeon/items/monsters.  Not the character or inventory.
+        public void saveInstance()
+        {
+            StreamWriter sr = new StreamWriter("Content/txt/" + name + ".txt");
+            sr.WriteLine(name);
+            sr.WriteLine(type);
+            sr.WriteLine(arrayOfLevels.Length);
+            for (int i = 0; i < arrayOfLevels.Length; i++)
+            {
+                sr.WriteLine(i + 1);
+                sr.WriteLine(arrayOfLevels[i].Size_Y + " " + arrayOfLevels[i].Size_X);
+                for (int k = 0; k < arrayOfLevels[i].board.GetLength(0); k++)
+                {
+                    for (int j = 0; j < arrayOfLevels[i].board.GetLength(1); j++)
+                    {
+                        sr.Write((int)arrayOfLevels[i].board[k, j] + "|");
+                    }
+                    sr.WriteLine();
+                }
+                /*sr.WriteLine("[Items]");
+                sr.WriteLine(arrayOfLevels[i].items.Length);
+                foreach (Item item in arrayOfLevels[i].items)
+                    sr.WriteLine(item.ToString(true));
+                sr.WriteLine("[Monsters]");
+                sr.WriteLine(arrayOfLevels[i].monsters.Count());
+                foreach (Monster monster in arrayOfLevels[i].monsters)
+                    sr.WriteLine(monster.ToString(true));*/
+            }
+            sr.Close();
+        }
+        public void loadInstance(string name)  //Loads the dungeon.
+        {
+            string[] data;  //for string.Split()'s
+            StreamReader sr = new StreamReader("Content/txt/" + name + ".txt");
+            this.name = sr.ReadLine();//load the name of the instance
+            if (sr.ReadLine().Equals("Cave")) //load the type of instance
+            {
+                type = DungeonType.Cave;
+            }
+            else
+            {
+                type = DungeonType.Dungeon;
+            }
+            arrayOfLevels = new CaveMap[int.Parse(sr.ReadLine())]; //load # of levels
+            for (int i = 0; i < arrayOfLevels.Length; i++)
+            {
+                sr.ReadLine(); //skip a line
+                data = sr.ReadLine().Split(' '); //Load the dimensions into the array
+                //Create a new CaveLevel of the saved dimensions
+                CaveMap cl = new CaveMap(int.Parse(data[0]), int.Parse(data[1]));
+                //and copy it into the array
+                arrayOfLevels[i] = cl;
+                for (int k = 0; k < arrayOfLevels[i].board.GetLength(0); k++)
+                {
+                    data = sr.ReadLine().Split('|');
+                    for (int j = 0; j < data.Length - 1; j++)
+                    {
+                        //Copies the actual map from the file
+                        arrayOfLevels[i].board[k, j] = (Map.Tile)int.Parse(data[j]);
+                    }
+                }
+                sr.ReadLine();//Skip a line
+                //Make a new array of items
+                /*arrayOfLevels[i].items = new Item[int.Parse(sr.ReadLine())];
+                for (int k = 0; k < arrayOfLevels[i].items.Length; k++)
+                {
+                    data = sr.ReadLine().Split('|');//AND MY AXE
+                    //Make a new item at the saved location
+                    Item item = new Item(int.Parse(data[12]), int.Parse(data[11]), true);
+                    //and then copy it into the array and set attributes from the file
+                    arrayOfLevels[i].items[k] = item;
+                    arrayOfLevels[i].items[k].Name = data[0];
+                    arrayOfLevels[i].items[k].AttackRatingAdd = int.Parse(data[1]);
+                    arrayOfLevels[i].items[k].DefenseRatingAdd = int.Parse(data[2]);
+                    arrayOfLevels[i].items[k].MagicRatingAdd = int.Parse(data[3]);
+                    arrayOfLevels[i].items[k].HitPointsAdd = int.Parse(data[4]);
+                    arrayOfLevels[i].items[k].ManaPointsAdd = int.Parse(data[5]);
+                    arrayOfLevels[i].items[k].STRadd = int.Parse(data[6]);
+                    arrayOfLevels[i].items[k].CONadd = int.Parse(data[7]);
+                    arrayOfLevels[i].items[k].DEXadd = int.Parse(data[8]);
+                    arrayOfLevels[i].items[k].INTadd = int.Parse(data[9]);
+                    arrayOfLevels[i].items[k].WISadd = int.Parse(data[10]);
+                }
+                sr.ReadLine();
+                int count = int.Parse(sr.ReadLine());
+                for (int k = 0; k < count; k++)
+                {
+                    data = sr.ReadLine().Split('|');
+                    arrayOfLevels[i].monsters.Add(new Monster(int.Parse(data[5]), int.Parse(data[6]), true));
+                    arrayOfLevels[i].monsters[k].Name = data[0];
+                    arrayOfLevels[i].monsters[k].HP = int.Parse(data[1]);
+                    arrayOfLevels[i].monsters[k].Speed = int.Parse(data[2]);
+                    arrayOfLevels[i].monsters[k].Attack = int.Parse(data[3]);
+                    arrayOfLevels[i].monsters[k].Level = int.Parse(data[4]);
+                }*/
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ Vale/Content/Map/Map.cs
@@ -0,0 +1,254 @@
+﻿//Spencer Corkran
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vale.Content.Map
+{
+    /*This class is a parent class*/
+    class Map
+    {
+        /*these attributes are common to all map types.  Dimensions,
+         * an array to hold tiles, and the flag enumeration for basic tile types.*/
+        //public Item[] items;
+        public Tile[,] board;
+        //public List<Monster> monsters = new List<Monster>();
+        private MersenneTwister rng = new MersenneTwister();
+
+        //g + b types are used to mark tiles as valid/invalid
+        //for various purposes.  Disjoints, items, monsters, etc.
+        [FlagsAttribute]
+        public enum Tile { f = 0x1, W = 0x2, g = 0x4, b = 0x8 };
+
+        //properties
+        private int badCount;
+        public int BadCount
+        {
+            get { return badCount; }
+            set { badCount = value; }
+        }
+
+        private int size_x;
+        public int Size_X { get { return size_x; } }
+        private int size_y;
+        public int Size_Y { get { return size_y; } }
+
+        private int objectX;
+        private int objectY;
+        //Constructor
+        public Map(int y, int x, bool load)
+        {
+            size_y = y;
+            size_x = x;
+            //if (load == true)
+                //items = new Item[rng.Next(Convert.ToInt32((Size_X + Size_Y) / 15))];
+        }
+        //Iterates through the array and
+        //prints each tile out into the console
+        public void printMap()
+        {
+            for (int k = 0; k < board.GetLength(0); k++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    switch ((int)board[k, j])
+                    {
+                        //changes floor tiles for the symbol '.'
+                        case 1:
+                            Console.Write('.');
+                            break;
+                        //changes wall tiles into '#'
+                        case 2:
+                            Console.Write('#');
+                            break;
+                        //down stairs into '>'
+                        case 9:
+                            Console.Write('>');
+                            break;
+                        //up stairs into '<'
+                        case 5:
+                            Console.Write('<');
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        /*Starts at a source tile, then spreads throughout adjacent floor
+         * tiles and marks them reachable if they are set to unreachable.*/
+        public void floodSearch(int y, int x)
+        {
+            if ((board[y, x] & Tile.f) == Tile.f)
+            {
+                if ((board[y, x] & Tile.b) == Tile.b)
+                {
+                    board[y, x] |= Tile.g;
+                    board[y, x] &= ~Tile.b;
+                }
+                else
+                    return;
+            }
+            else
+                return;
+            /*Recursively propagates to each square around it.  Currently spawns in
+             * the surrounding cardinal squares.*/
+            floodSearch(y, x + 1);
+            floodSearch(y, x - 1);
+            floodSearch(y + 1, x);
+            floodSearch(y - 1, x);
+        }
+        /* Checks to see if floodFill() could reach every floor tile.
+         * returns true if it reached every floor tile and false
+         * if the map was disjointed and floodFill() missed a tile*/
+        public bool isEverythingReachable()
+        {
+            BadCount = 0;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if ((board[i, j] & Tile.b) == Tile.b)
+                    {
+                        if ((board[i, j] & Tile.f) == Tile.f)
+                        {
+                            /* While it's at it, it counts the unreachable
+                             * floor tiles.  If the number is too large,
+                             * a map should be discarded*/
+                            badCount++;
+                        }
+                    }
+                }
+            }
+            if (badCount > 0)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+        /*Iterates through the map until it finds a floor tile,
+         * then sends its index in the array back.  This
+         * provides the starting tile for floodFill().*/
+        public int[] findFloor()
+        {
+            int[] index = new int[2];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if ((board[i, j] & Tile.f) == Tile.f)
+                    {
+                        index[0] = i;
+                        index[1] = j;
+                        return index;
+                    }
+                }
+            }
+            return index; //If this happens something went wrong
+        }
+        /* Changes any unreachable floor into a wall*/
+        public void fill()
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if ((board[i, j] & Tile.f) == Tile.f)
+                    {
+                        if ((board[i, j] & Tile.b) == Tile.b)
+                        {
+                            board[i, j] |= Tile.W;
+                            board[i, j] &= ~Tile.f;
+                        }
+                    }
+                }
+            }
+        }
+        /* Sets all tiles to "unreachable"*/
+        public void floodPrep()
+        {
+            //Sets every tile to 'bad', or 'unreachable'
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] |= Tile.b;
+                }
+            }
+        }
+        public void clean()
+        {
+            //Turns off g + b enums
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] &= ~Tile.b;
+                    board[i, j] &= ~Tile.g;
+                }
+            }
+        }
+        /*This method keeps making duplicates
+         * it should be threaded so we can pause it
+         * and get unique items each time*/
+        /*public void placeObject()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                bool place = false;
+                //If it finds a bad spot, keep trying! 
+                while (place == false)
+                {
+                    objectY = rng.Next(1, Size_Y - 1);
+                    objectX = rng.Next(1, Size_X - 1);
+                    if ((board[objectY, objectX] & Tile.f) == Tile.f)
+                    {
+                        //generate an item
+                        items[i] = new Item(objectY, objectX, false);
+                        place = true;
+                    }
+                }
+            }
+            for (int i = 0; i < rng.Next(Convert.ToInt32((Size_X + Size_Y) / 20), Convert.ToInt32((Size_X + Size_Y) / 15)); i++)
+            {
+                bool place = false;
+                while (place == false)
+                {
+                    objectY = rng.Next(1, Size_Y - 1);
+                    objectX = rng.Next(1, Size_X - 1);
+                    if ((board[objectY, objectX] & Tile.f) == Tile.f)
+                    {
+                        //generate a monster
+                        monsters.Add(new Monster(objectX, objectY, false));
+                        place = true;
+                    }
+                }
+            }
+            clean();
+            bool good = false;
+            bool alt = false;
+            //If it finds a bad spot, keep trying
+            // TODO: does not check if good spots exist, so the program hangs on small/filled maps.
+            while (good == false)
+            {
+                objectY = rng.Next(1, Size_Y - 1);
+                objectX = rng.Next(1, Size_X - 1);
+                if (alt == false && (int)board[objectY, objectX] == 1)
+                {
+                    //f|b can be used to create down stairs I suppose
+                    board[objectY, objectX] |= Tile.b;
+                    alt = true;
+                }
+                if (alt == true && (int)board[objectY,objectX] == 1)
+                {
+                    //f|g can be up stairs
+                    board[objectY, objectX] |= Tile.g;
+                    good = true;
+                }
+            }
+        }*/
+    }
+}
