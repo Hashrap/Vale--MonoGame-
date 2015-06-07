4e8417776962b9cadba10b25b3f8a7775bf6217b
Josaphat Valdivia <josaphat.valdivia@gmail.com>
2015-06-07 01:44:35 -0400

    Add a GameObject base class for update and drawing.
    This makes objects that get drawn or updated polymorphically the
    same and they can be moved around and iterated over easily (rather
    than having to explicitly use the Hero object, as well as each
    projectile, tile, map, etc. They can all be treated the same when
    appropriate.
    
    The motiviation for this addition is primarly to allow GameObjects
    to exist at the top-most level of the GameplayScreen. This will let
    the GameplayScreen update and draw the objects it cares about.

---
+++ Vale/GameObjects/GameObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Vale.ScreenSystem.Screens;
+
+namespace Vale.GameObjects
+{
+    public abstract class GameObject
+    {
+        public GameplayScreen Game { get; set; }
+
+        public GameObject(GameplayScreen game)
+        {
+            this.Game = game;
+        }
+
+
+        public virtual void LoadContent(ContentManager content)
+        {
+            // Nothing~
+        }
+
+        public virtual void UnloadContent(ContentManager content)
+        {
+            // Nothing~
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            // Nothing~
+        }
+
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // Nothing~
+        }
+    }
+}
