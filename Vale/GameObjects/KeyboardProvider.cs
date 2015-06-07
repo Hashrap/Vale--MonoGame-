845e9587b5861510ebb7b1635eba25be4f8d2119
Josaphat Valdivia <josaphat.valdivia@gmail.com>
2015-06-07 01:38:18 -0400

    Add GameObject wrappers for keyboard and mouse input.
    This allows the objects to be automatically updated by iterating in
    the GameplayScreen and is more extensible. For instance, if the game
    becomes networked in the future, checking whether a remote player
    performed an action does not check the keyboard and mouse on their
    system, it queries data obtained from the network.

---
+++ Vale/GameObjects/KeyboardProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Vale.ScreenSystem.Screens;
+
+namespace Vale.GameObjects
+{
+    public class KeyboardProvider : GameObject
+    {
+        public KeyboardProvider(GameplayScreen game)
+            : base(game)
+        {
+        }
+
+        public bool KeyPress(char key)
+        {
+            return Vale.Control.Input.Instance.KeyPress((Keys)char.ToUpper(key));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+    }
+}
---
+++ Vale/GameObjects/MouseProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Vale.ScreenSystem.Screens;
+
+namespace Vale.GameObjects
+{
+    /// <summary>
+    /// An object which provides the position of the mouse-pointer in screen-
+    /// coordinates. This could be extended to be a "pointer provider" which can
+    /// be updated by either the mouse or by a gamepad-controlled cursor.
+    /// </summary>
+    public class MouseProvider : GameObject
+    {
+        public enum Button {
+            LMB,
+            RMB,
+            MMB,
+        };
+
+        /// <summary>
+        /// The position of the pointer in world-space coordinates.
+        /// </summary>
+        /// <value>The pointer position in world-space coordinates.</value>
+        public Vector2 PointerPosition { get; private set; }
+
+        public MouseProvider(GameplayScreen game)
+            : base(game)
+        {
+        }
+
+        public bool ButtonPress(Button button)
+        {
+            Vale.Control.Input.MouseButtons which_button;
+            switch (button)
+            {
+                case Button.LMB:
+                    which_button = Vale.Control.Input.MouseButtons.Left;
+                    break;
+                case Button.RMB:
+                    which_button = Vale.Control.Input.MouseButtons.Right;
+                    break;
+                case Button.MMB:
+                    which_button = Vale.Control.Input.MouseButtons.Middle;
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+            return Control.Input.Instance.MouseButtonPress(which_button);
+        }
+
+        /// <summary>
+        /// Update the pointer position.
+        /// </summary>
+        /// <param name="gameTime">Game time.</param>
+        public override void Update(GameTime gameTime)
+        {
+            PointerPosition = Game.camera.ScreenToWorldCoords(Control.Input.Instance.MousePosition);
+        }
+    }
+}
